44d9885ceab65594ef460f87e393f23f4b722ad7
Miron <miron.kostenko@gmail.com>
2025-11-01 21:28:45 +0100

    again

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/pixel-cards/",
+  build: {
+    target: ["es2018", "safari13"],
+    polyfillModulePreload: true,
+  },
 });
